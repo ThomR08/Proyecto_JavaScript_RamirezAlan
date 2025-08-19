8a4ce5b5da7e80d2a0c0ccff9bf734d8d1d52a6b
Alan Thomas Ramirez Vargas (Termux tab A9+) <thomasramirez0806@gmail.com>
2025-08-19 12:32:22 -0500

    feat :sparkles: get form mail and password

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 import * as modal from './functions.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    let user = await modal.fetchAUser(1);
-    let imgProfile = document.getElementById("imgProfile");
-    imgProfile.src = user.imageUrl;
+  console.log(1)
+  document.querySelector('form')
+    .addEventListener('submit', async e => {
+      e.preventDefault()
+      const data = Object.fromEntries(
+        new FormData(e.target)
+      );
+      alert(JSON.stringify(data))
+    });
+  let user = await modal.fetchAUser(1);
+  let imgProfile = document.getElementById("imgProfile");
+  imgProfile.src = user.imageUrl;
 });
