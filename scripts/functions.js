2ff3fdf5fb4969a391033ba99f48ad93eb7e3c32
Alan Thomas Ramirez Vargas <thomasramirez0806@gmail.com>
2025-08-24 13:20:52 -0500

    feat :sparkles: Developed startContent()

--- scripts/functions.js
+++ scripts/functions.js
@@ -50,11 +50,37 @@ export async function authenticate() {
         if (authenticator.status === "authenticated") {
             localStorage.setItem('authenticator', JSON.stringify(authenticator));
             const signInContain = document.getElementById("signInContain");
+            document.getElementById("dashboard").addEventListener('click', e => {
+                e.preventDefault()
+                animation()
+                startContent()
+            });
+            document.getElementById("courses").addEventListener('click', e => {
+                e.preventDefault()
+                animation()
+                startCourses()
+            });
+            document.getElementById("assignments").addEventListener('click', e => {
+                e.preventDefault()
+                animation()
+                startAssignments()
+            });
+            document.getElementById("teachers").addEventListener('click', e => {
+                e.preventDefault()
+                animation()
+                startTeachers()
+            });
+            document.getElementById("profile").addEventListener('click', e => {
+                e.preventDefault()
+                animation()
+                startProfile()
+            });
             const main = document.getElementById("main");
             signInContain.classList.remove("active");
             signInContain.classList.add("hidden");
             main.classList.remove("hidden");
             main.classList.add("grid");
+            startContent()
         };
     };
 };
@@ -66,17 +92,105 @@ export async function startContent() {
         const imgProfile = document.getElementById("imgProfile");
         imgProfile.src = userApi.imageUrl;
         const main = document.querySelector("main");
-        main.innerHTML = ""
+        main.innerHTML = "";
         if (userApi.role === "student") {
-            
-            const dashboard = document.createElement("section")
-            dashboard.append(
-                Object.assign(document.createElement("h1"), {textContent: "Dashboard"}),
-                Object.assign(document.createElement("div").append(
-                    Object.assign(document.createElement("div"), {className: "cardContent"})
-                ), {className: "card"})
-            );
+            const numberActiveCourses = userApi.enrolledCourses.length // Primera data
+
+            const submittedAssignments = await fetchUserSubmittedAssignments(userApi.id)
+            let numberPendingAssignments = 0;
+            submittedAssignments.forEach(element => {
+                if (!element.done) {
+                    numberPendingAssignments += 1;
+                }
+            }); // Segunda data
+
+            let doneCourses = 0;
+            userApi.enrolledCourses.forEach(element => {
+                let lessons = element.lessons.length;
+                let doneLessons = 0;
+                element.lessons.forEach(element => {
+                    if (element.done) {
+                        doneLessons += 1;
+                    }
+                });
+                doneCourses += (doneLessons / lessons);
+            });
+            const progress = Math.floor(doneCourses / userApi.enrolledCourses.length * 100); // Tercera data
+
+            const sectionCourses = document.createElement("section");
+            sectionCourses.innerHTML = `
+        <section>
+            <div class="space-between">
+                <h2>My Enrolled Courses</h2>
+                <button class="BrowseMoreCourses">Browse More Courses</button>
+            </div>
+            <div class="row">
+            </div>
+        </section>
+            `
+            const row = sectionCourses.querySelector(".row");
+            let counter = 0;
+            for (const element of userApi.enrolledCourses) {
+                if (counter > 2) {
+                    break;
+                }
+
+
+                let doneLessons = 0;
+                userApi.enrolledCourses[counter].lessons.forEach(element => {
+                    if (element.done) {
+                        doneLessons += 1;
+                    }
+                });
+                const apiCourse = await fetchACourse(element.courseId);
+                const apiTeacher = await fetchAUser(apiCourse.teacherInChargeId);
+                const progress = Math.floor(doneLessons / apiCourse.lessons.length * 100);
+                row.innerHTML += `
+                <div class="card">
+                    <img src="${apiCourse.imageUrl}" alt="">
+                    <div class="cardContent">
+                        <span class="t2">${apiCourse.name}</span>
+                        <span class="t5">${apiTeacher.name}</span>
+                        <div class="row2">
+                            <progress max="100" value="${progress}"></progress>
+                            <span class="t5">${progress}% Complete</span>
+                            <span class="t3">${doneLessons}/${apiCourse.lessons.length} Lessons</span>
+                        </div>
+                    </div>
+                    <button class="continueLearning" courseId="${apiCourse.id}">Continue Learning</button>
+                </div>
+                `
+                counter += 1;
+            }
+            const dashboard = document.createElement("section");
+            dashboard.innerHTML = `
+            <h1>Dashboard</h1>
+            <div class="row">
+                <div class="card">
+                    <div class="cardContent">
+                        <h3>Active Courses</h3>
+                        <h3 id="numberActiveCourses">${numberActiveCourses}</h3>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="cardContent">
+                        <h3>Pending Assignments</h3>
+                        <h3 id="numberPendingAssignments">${numberPendingAssignments}</h3>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="cardContent">
+                        <h3>Overall Progress</h3>
+                        <div class="row2">
+                            <progress max="100" value="${progress}"></progress>
+                            <h3>${progress}%</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
             main.appendChild(dashboard);
+            main.appendChild(sectionCourses);
         } else if (userApi.role === "teacher") {
         } else if (userApi.role === "admin") {
         }
@@ -94,8 +208,41 @@ export async function fetchCourses() {
     return data;
 };
 
+export async function fetchACourse(id) {
+    const response = await fetch(`https://68a09e396e38a02c58193795.mockapi.io/courses/${id}`, {
+        method: 'GET',
+        headers: {
+            'content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+};
+
 export async function fetchAssignments() {
     const response = await fetch(`https://68a09e396e38a02c58193795.mockapi.io/assignments`, {
+        method: 'GET',
+        headers: {
+            'content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+};
+
+export async function fetchSubmittedAssignments() {
+    const response = await fetch(`https://68a1ebfa6f8c17b8f5db1b38.mockapi.io/submittedAssignments`, {
+        method: 'GET',
+        headers: {
+            'content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+};
+
+export async function fetchUserSubmittedAssignments(id) {
+    const response = await fetch(`https://68a1ebfa6f8c17b8f5db1b38.mockapi.io/users/${id}/submittedAssignments`, {
         method: 'GET',
         headers: {
             'content-Type': 'application/json'
@@ -140,4 +287,19 @@ export async function fetchAUser(id) {
 
 export async function studentDashboard() {
     const main = document.getElementByTagName("main")
+}
+
+export function animation() {
+    const logoContain = document.getElementById("logoContain");
+    const startupLogo = document.getElementById("startupLogo");
+    logoContain.classList.remove("active");
+    startupLogo.classList.remove("active");
+    logoContain.classList.add("hidden");
+    startupLogo.classList.add("hidden");
+    setTimeout(async () => {
+        logoContain.classList.remove("hidden");
+        startupLogo.classList.remove("hidden");
+        logoContain.classList.add("active");
+        startupLogo.classList.add("active");
+    }, 50)
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 import * as modal from './functions.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const logoContain = document.getElementById("logoContain");
-  const startupLogo = document.getElementById("startupLogo");
   modal.authenticate()
   document.querySelector('form')
     .addEventListener('submit', async e => {
@@ -14,16 +12,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       authenticator.userEmail = data.email;
       authenticator.userPassword = data.password;
       localStorage.setItem('authenticator', JSON.stringify(authenticator));
-      logoContain.classList.remove("active");
-      startupLogo.classList.remove("active");
-      logoContain.classList.add("hidden");
-      startupLogo.classList.add("hidden");
-      setTimeout(async () => {
-        logoContain.classList.remove("hidden");
-        startupLogo.classList.remove("hidden");
-        logoContain.classList.add("active");
-        startupLogo.classList.add("active");
-      }, 50)
+      animation()
       await modal.authenticate()
     });
 });
